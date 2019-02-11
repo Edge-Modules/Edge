f4649e50e9ff963c25192eb0544c5d491c62a69f
Edge <mayatito51@hotmail.com>
2019-02-11 14:46:58 +0100

    Signed-off-by: Edge <mayatito51@hotmail.com>

---
+++ build/src/WTFDoveVCO.cpp.d
@@ -0,0 +1,75 @@
+build/src/WTFDoveVCO.cpp.o: src/WTFDoveVCO.cpp src/Edge.hpp \
+ ../../include/rack.hpp ../../include/util/common.hpp \
+ ../../include/util/math.hpp ../../include/asset.hpp \
+ ../../include/plugin.hpp ../../include/tags.hpp ../../include/engine.hpp \
+ ../../dep/include/jansson.h ../../dep/include/jansson_config.h \
+ ../../include/widgets.hpp ../../dep/include/nanovg.h \
+ ../../dep/include/nanosvg.h ../../include/events.hpp \
+ ../../include/util/color.hpp ../../include/app.hpp ../../include/ui.hpp \
+ ../../dep/include/blendish.h ../../include/componentlibrary.hpp \
+ ../../include/helpers.hpp ../../include/dsp/resampler.hpp \
+ ../../dep/include/speex/speex_resampler.h \
+ ../../dep/include/speex/speexdsp_types.h ../../include/dsp/frame.hpp \
+ ../../include/dsp/ringbuffer.hpp ../../include/dsp/fir.hpp \
+ ../../include/dsp/functions.hpp ../../dep/include/pffft.h \
+ ../../include/dsp/filter.hpp src/Edge_Component.hpp
+
+src/Edge.hpp:
+
+../../include/rack.hpp:
+
+../../include/util/common.hpp:
+
+../../include/util/math.hpp:
+
+../../include/asset.hpp:
+
+../../include/plugin.hpp:
+
+../../include/tags.hpp:
+
+../../include/engine.hpp:
+
+../../dep/include/jansson.h:
+
+../../dep/include/jansson_config.h:
+
+../../include/widgets.hpp:
+
+../../dep/include/nanovg.h:
+
+../../dep/include/nanosvg.h:
+
+../../include/events.hpp:
+
+../../include/util/color.hpp:
+
+../../include/app.hpp:
+
+../../include/ui.hpp:
+
+../../dep/include/blendish.h:
+
+../../include/componentlibrary.hpp:
+
+../../include/helpers.hpp:
+
+../../include/dsp/resampler.hpp:
+
+../../dep/include/speex/speex_resampler.h:
+
+../../dep/include/speex/speexdsp_types.h:
+
+../../include/dsp/frame.hpp:
+
+../../include/dsp/ringbuffer.hpp:
+
+../../include/dsp/fir.hpp:
+
+../../include/dsp/functions.hpp:
+
+../../dep/include/pffft.h:
+
+../../include/dsp/filter.hpp:
+
+src/Edge_Component.hpp:
